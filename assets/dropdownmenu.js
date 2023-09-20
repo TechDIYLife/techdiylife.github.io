1b91ec5d5657d619a7e4ec55f0a410238254244e
TechDL <126927944+TechDIYLife@users.noreply.github.com>
2023-09-20 16:52:48 +0900

    Add files via upload

--- assets/dropdownmenu.js
+++ assets/dropdownmenu.js
@@ -1,22 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //DS-Chat menu
   const dropdownMenu = document.getElementById("dropdown-menu");
-  const dropdownContent = document.createElement("div");
-  dropdownContent.className = "dropdown";
+  if (dropdownMenu!=null){
+    const dropdownContent = document.createElement("div");
+    dropdownContent.className = "dropdown";
 
-  const menuItems = [
-    { text: "第1章：DeepSpeed-Chat训练实战", href: "/big-model-training/deepspeed/deepspeed-chat.html" },
-    { text: "第2章：DeepSpeed-Chat代码分析", href: "/big-model-training/deepspeed/deepspeed-chat-code.html" },
-    { text: "第3章：DeepSpeed-Chat 模型与数据", href: "/big-model-training/deepspeed/deepspeed-chat-model-data.html" },
-    { text: "第4章：跟大牛学LLM训练和使用技巧", href: "/big-model-training/deepspeed/LLM-state-of-GPT.html" },
-    { text: "第5章：HF Transformers Generate功能介绍", href: "/big-model-training/deepspeed/HF-transformers-generate.html" },
-  ];
+    const menuItems = [
+      { text: "第1章：DeepSpeed-Chat训练实战", href: "/big-model-training/deepspeed/deepspeed-chat.html" },
+      { text: "第2章：DeepSpeed-Chat代码分析", href: "/big-model-training/deepspeed/deepspeed-chat-code.html" },
+      { text: "第3章：DeepSpeed-Chat 模型与数据", href: "/big-model-training/deepspeed/deepspeed-chat-model-data.html" },
+      { text: "第4章：跟大牛学LLM训练和使用技巧", href: "/big-model-training/deepspeed/LLM-state-of-GPT.html" },
+      { text: "第5章：HF Transformers Generate功能介绍", href: "/big-model-training/deepspeed/HF-transformers-generate.html" },
+    ];
+    menuItems.forEach((item) => {
+      const link = document.createElement("a");
+      link.textContent = item.text;
+      link.href = item.href;
+      dropdownContent.appendChild(link);
+    });
 
-  menuItems.forEach((item) => {
-    const link = document.createElement("a");
-    link.textContent = item.text;
-    link.href = item.href;
-    dropdownContent.appendChild(link);
-  });
+    dropdownMenu.appendChild(dropdownContent);
+  }
+  
+  //AI-game
+  const dropdownMenuAIGame = document.getElementById("dropdown-menuAIGame");
+  if (dropdownMenuAIGame!=null){
+    const dropdownContent = document.createElement("div");
+    dropdownContent.className = "dropdown";
+    const menuItems = [
+      { text: "第1章：AI挑战贪吃蛇专题介绍", href: "/AI-Game/snake/01-AI-snake-intro.html" },
+      { text: "第2章：AI贪吃蛇环境安装", href: "/AI-Game/snake/02-snake-ai-install.html" },
+    ];
+    menuItems.forEach((item) => {
+      const link = document.createElement("a");
+      link.textContent = item.text;
+      link.href = item.href;
+      dropdownContent.appendChild(link);
+    });
 
-  dropdownMenu.appendChild(dropdownContent);
+    dropdownMenuAIGame.appendChild(dropdownContent);
+  }
 });
