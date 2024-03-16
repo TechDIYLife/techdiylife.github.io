777f7e842eb45b4c6a6d658de3a7a08976156af6
Tech <126927944+TechDIYLife@users.noreply.github.com>
2024-03-16 22:34:02 +0900

    infer

--- blog/assets/blog_list.js
+++ blog/assets/blog_list.js
@@ -55,12 +55,14 @@ const blogList = [
   { id: "0030", category1: "c01", category2: "", create_date: "2024.03.09", title_short: "开源大模型下载量排行榜（2024年3月更新版）", title_full: "开源大模型下载量排行榜（2024年3月更新版）", href: "202401/240309-LLM-OpenModels.html", video_href: "" },
   { id: "0031", category1: "c02", category2: "t05", create_date: "2024.03.13", title_short: "如何知道一个大模型在推理和训练时需要多少显存？", title_full: "如何知道一个大模型在推理和训练时需要多少显存？", href: "202401/240313-LLM-Memory.html", video_href: "" },
   { id: "0032", category1: "c05", category2: "", create_date: "2024.03.14", title_short: "AI共学会：LLM部署实战交流", title_full: "AI共学会：LLM部署实战交流", href: "202401/240314-gongxue-bushu-1.html", video_href: "" },
+  { id: "0033", category1: "c02", category2: "t05", create_date: "2024.03.16", title_short: "大模型推理技巧：分层推理法", title_full: "大模型推理技巧：分层推理法", href: "202401/240316-LLM-Inference.html", video_href: "" },
   
   
   
 ];
 
 const NeWvideoList = [
+  { memo: "分层推理法", href: "https://www.youtube.com/embed/A5cxHkRBx7o?si=3SCb77e_yk4H8Ebm" },
   { memo: "Estimate Memory", href: "https://www.youtube.com/embed/Z9eV9mbB6uY?si=ZXnKk11NWNNG4jjv" },
   { memo: "LLM rank", href: "https://www.youtube.com/embed/Ur65L4iAM4o?si=nnbANprNJjFznOVp" },
   { memo: "Auto-map", href: "https://www.youtube.com/embed/rIk0OWz3tsc?si=SGrFJIizKUGm_iis" },
