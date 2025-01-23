8bf3370cdb8d66b564b11709c707d39750f6811a
Tech <126927944+TechDIYLife@users.noreply.github.com>
2025-01-23 23:21:20 +0900

    new

--- blog/assets/blog_list.js
+++ blog/assets/blog_list.js
@@ -99,8 +99,8 @@ const blogList = [
   { id: "0070", category1: "c01", category2: "", create_date: "2024.12.05", title_short: "2024年LLM进展回顾（Ollama视角）", title_full: "2024年LLM进展回顾（Ollama视角）", href: "202401/241204-LLM-trends-ollama.html", video_href: "" },
   { id: "0071", category1: "c02", category2: "", create_date: "2024.12.20", title_short: "LlamaIndex AI开发实践共学专题", title_full: "LlamaIndex AI开发实践共学专题", href: "202401/241218-llamaindex.html", video_href: "" },
   { id: "0072", category1: "c02", category2: "", create_date: "2024.12.25", title_short: "性能实测：顶流开源vs顶流闭源", title_full: "性能实测：顶流开源vs顶流闭源", href: "202401/241212-llm-eval.html", video_href: "" },
+  { id: "0073", category1: "c02", category2: "", create_date: "2025.01.23", title_short: "搞懂MoE:从概念到代码全给你整明白", title_full: "搞懂MoE:从概念到代码全给你整明白", href: "202501/250118-MoE.html", video_href: "" },
 
-  
 ];
 
 const NeWvideoList = [
