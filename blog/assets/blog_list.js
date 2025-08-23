97cfe7975d0f95574ac5a57fcae7a0d43e584787
TechDL <126927944+TechDIYLife@users.noreply.github.com>
2025-08-23 17:44:55 +0900

    up

--- blog/assets/blog_list.js
+++ blog/assets/blog_list.js
@@ -110,6 +110,8 @@ const blogList = [
 
   { id: "0080", category1: "c02", category2: "t10", create_date: "2025.08.19", title_short: "快速上手 LLM 演示：Gradio AI演示程序入门教程", title_full: "Gradio AI演示程序入门教程", href: "202501/gradio_demo_chapter0.html", video_href: "" },
   { id: "0081", category1: "c02", category2: "t10", create_date: "2025.08.20", title_short: "10个问题快速了解 Gradio", title_full: "10个问题快速了解 Gradio", href: "202501/gradio_demo_chapter1.html", video_href: "" },
+  { id: "0082", category1: "c02", category2: "t10", create_date: "2025.08.22", title_short: "翻译Demo：使用 Gradio + vLLM 搭建中英翻译应用", title_full: "使用 Gradio + vLLM 搭建中英翻译应用", href: "202501/gradio_demo_chapter2.html", video_href: "" },
+  { id: "0083", category1: "c02", category2: "t10", create_date: "2025.08.23", title_short: "翻译Demo Streaming 版：如何实现像 ChatGPT 一样输出", title_full: "翻译Demo Streaming 版", href: "202501/gradio_demo_chapter3.html", video_href: "" },
 
 
 ];
